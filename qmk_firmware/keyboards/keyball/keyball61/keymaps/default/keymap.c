56c9f2b336164a2b79254a89d57f1a917eed6a1a
NichtEuler <horrerri67684@th-nuernberg.de>
2025-01-31 13:29:52 +0100

    numpad

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
@@ -42,11 +42,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_universal(
-    SSNP_FRE , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    SSNP_VRT , _______  , KC_7     , KC_8     , KC_9     , KC_BSLS  ,                                  _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
-    SSNP_HOR , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , _______  ,
-    _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), KC_QUOT  ,            S(KC_QUOT)  , KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU  , KBC_SAVE  ,
-    _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,            KC_BSPC  , KC_DEL   , _______  , _______  , _______  , KC_BSLS  , _______
+    _______ , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+    _______ , KC_PMNS  , KC_P7    , KC_P8    , KC_P9     , KC_BSLS  ,                                  _______  , KC_LEFT  , KC_UP    , KC_RGHT  , _______  , KC_F12   ,
+    _______ , KC_PSLS  , KC_P4     , KC_P5     , KC_P6     ,S(KC_SCLN),                                  KC_PGUP  , KC_BTN1  , KC_DOWN  , KC_BTN2  , KC_BTN3  , _______  ,
+    _______  , KC_PPLS  , KC_P1     , KC_P2     , KC_P3     ,KC_BSLS   , KC_QUOT  ,            S(KC_QUOT)  , KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU  , KBC_SAVE  ,
+    _______  , KC_PAST  , KC_0     , KC_0   , KC_DOT  , _______  , _______  ,            KC_BSPC  , KC_DEL   , _______  , _______  , _______  , KC_BSLS  , _______
   ),
   [2] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL  , KC_LGUI  , KC_DEL   , KC_LALT  , KC_LCTL , KC_SPC    , KC_TAB,                KC_BSPC ,  LT(1,KC_ENT),LT(1,KC_LNG2),KC_RGUI, _______ , KC_RALT  , KC_PSCR
   ),
   [3] = LAYOUT_universal(
-    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , _______  ,                                  RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , _______  ,                                  RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  , SSNP_FRE  ,                                  RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SSNP_VRT  ,                                  RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SSNP_HOR  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
     _______  , _______  , SCRL_DVD , SCRL_DVI , SCRL_MO  , SCRL_TO  , EE_CLR   ,            EE_CLR   , _______ , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END    , _______  ,
     QK_BOOT  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            KC_BSPC  , KC_DEL  , _______  , _______  , _______  , _______  , QK_BOOT
   ),
