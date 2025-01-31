c27edce9403d55b97d2ee9d0e9033de3581e2b17
NichtEuler <horrerri67684@th-nuernberg.de>
2025-01-31 14:18:00 +0100

    v

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
@@ -36,14 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_DEL   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL  ,
-    KC_TAB   , LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                           KC_H     , LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),  LGUI_T(KC_SCLN)  , TG(1)  ,
+    KC_TAB   , LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                           KC_H     , LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),  LGUI_T(KC_SCLN)  , TO(1)  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,                       KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
     KC_LCTL  , KC_LGUI  ,LT(4, KC_DEL), KC_LALT ,LT(2, KC_UP),LT(2,KC_SPC),LT(3, KC_TAB),             KC_BSPC,  LT(2,KC_ENT),LT(3,KC_LNG2),KC_RGUI, _______ , KC_RALT  , KC_PSCR
   ),
   [1] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_DEL   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_INT3  ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , TO(0)  ,
+    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , TG(0)  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,                       KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
     KC_LCTL  , KC_LGUI  , KC_DEL   , KC_LALT  , MO(2) , KC_SPC    , KC_TAB,                KC_BSPC ,  LT(2,KC_ENT),LT(2,KC_LNG2),KC_RGUI, _______ , KC_RALT  , KC_PSCR
   ),
