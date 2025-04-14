705781fcb916f3fa76c970c47dc8ab7efb3febb7
NichtEuler <horrerri67684@th-nuernberg.de>
2025-04-14 10:10:05 +0200

    dd

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/keymap.c
@@ -71,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_BOOT  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            KC_BSPC  , KC_DEL  , _______  , _______  , _______  , KC_DEL  , QK_BOOT
   ),
   [4] = LAYOUT_universal(
-    KC_GRV  , KC_F1    , KC_F2    , KC_F3    , KC_F4     , KC_F5    ,                                     KC_F6    ,    KC_F7  , KC_F8    , KC_F9 , KC_F10   , KC_NUM   ,
-    _______  , KC_F11  , KC_F12  , _______  , _______  , _______  ,                                     _______   ,  KC_P7    , KC_P8    , KC_P9  ,KC_PMNS  , KC_CALC,
-    _______ , _______  , _______  , _______  , _______  , _______  ,                                  KC_DOT    ,  KC_P4     , KC_P5   , KC_P6  ,KC_PSLS  ,_______,
-    _______  , _______  , _______ , _______ , _______  , _______  , _______   ,              _______   , KC_0  ,  KC_P1     , KC_P2   , KC_P3  ,KC_PPLS  ,_______,
-    _______  , _______  , _______  , _______  , _______    , _______  , _______  ,              KC_BSPC  , KC_DEL    ,  KC_DOT     , KC_0   , KC_DEL ,KC_PAST  ,_______
+    KC_GRV  , _______    , _______    , KC_PAST    , KC_PSLS     , _______    ,                    _______    ,    _______  , _______    , _______ , _______   , KC_NUM   ,
+    _______  , _______  , KC_P7    , KC_P8    , KC_P9   , KC_PMNS  ,                                     _______   ,  KC_P7    , KC_P8    , KC_P9  ,KC_PMNS  , KC_CALC,
+    _______ , _______  ,KC_P4     , KC_P5   , KC_P6  , KC_PPLS  ,                                  KC_DOT    ,  KC_P4     , KC_P5   , KC_P6  ,KC_PSLS  ,_______,
+    _______  , _______  , KC_P1     , KC_P2   , KC_P3  , _______  , _______   ,              _______   , KC_0  ,  KC_P1     , KC_P2   , KC_P3  ,KC_PPLS  ,_______,
+    _______  , _______  , _______  , KC_DOT     , KC_0    , _______  , _______  ,              KC_BSPC  , KC_DEL    ,  KC_DOT     , KC_0   , KC_DEL ,KC_PAST  ,_______
   ),
 
 };
