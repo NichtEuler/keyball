cf2bb1557ab5381b0e90b550a4f3245630e72b5c
NichtEuler <horrerri67684@th-nuernberg.de>
2025-07-18 12:02:32 +0200

    tapping term

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
@@ -30,11 +30,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
 
-#define TAPPING_TERM 120
-#define TAPPING_TERM_PER_KEY
-// used for Tapping Term on thumb keys
-#define TAPPING_TERM_THUMB 200
+// default but used in macros
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 35
+#define QUICK_TAP_TERM 0
 #define SPLIT_USB_DETECT
 #define WAIT_FOR_USB
