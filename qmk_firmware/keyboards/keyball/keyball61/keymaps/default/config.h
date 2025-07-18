1a1a64d8cf1481a1b15bb3580b6d8ac065da1ef7
NichtEuler <horrerri67684@th-nuernberg.de>
2025-07-18 12:04:47 +0200

    update tapping term

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
@@ -33,9 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // default but used in macros
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
 #define SPLIT_USB_DETECT
